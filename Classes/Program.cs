982b543b1cd3feb380b3240f3f97fb950960594c
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-29 12:22:37 -0400

    Finished Exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Corola S";
+            myCar.Year = 2012;
+
+            Console.WriteLine(myCar.Make);
+            Console.WriteLine(myCar.Model);
+            Console.WriteLine(myCar.Year);
         }
     }
 }
